e41b66d8aadc1c66b7e97db79e80b87d180fd753
Pablo J <olbapjose@gmail.com>
2025-02-02 23:20:21 +0100

    Implementado test y añadida llamada a preprocesamiento a clase Featurizer

--- tests/test_featurizer.py
+++ tests/test_featurizer.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 from pyspark.sql import functions as F, SparkSession
 
@@ -8,10 +9,23 @@ def test_preprocesa(spark: SparkSession):
     df = spark.createDataFrame(
         [("2025-01-31", 1035, 0.0, 35.0, None, None, None, None),
          ("2025-02-01", 830, 1.0, None, None, None, None, None)],
-        ["FlightDate", "DepTime", "Diverted",
-        "CarrierDelay", "WeatherDelay", "NASDelay", "SecurityDelay", "LateAircraftDelay"]
+        schema="FlightDate string, DepTime int, Diverted float, " +
+               "CarrierDelay float, WeatherDelay float, NASDelay float, SecurityDelay float, LateAircraftDelay float"
     )
 
     featurizer = Featurizer(spark, {})
     preprocesado_df = featurizer.preprocesa(df)
 
+    f1, f2 = preprocesado_df.collect()
+
+    assert(f1.Diverted is False and
+           f1.WeatherDelay == 0.0 and f1.NASDelay == 0.0 and f1.SecurityDelay == 0.0 and f1.LateAircraftDelay == 0.0)
+
+    assert(f2.Diverted is True and f2.CarrierDelay == 0.0 and
+           f2.WeatherDelay == 0.0 and f2.NASDelay == 0.0 and f2.SecurityDelay == 0.0 and f2.LateAircraftDelay == 0.0)
+
+    assert(preprocesado_df.dtypes[-1] == ("FlightTs", "timestamp"))  # la última pareja de la lista de tipos
+    assert(f1.FlightTs == datetime(2025, 1, 31, 10, 35))
+    assert(f2.FlightTs == datetime(2025, 2, 1, 8, 30))
+
+    preprocesado_df.show()
