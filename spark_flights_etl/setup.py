d5d1a33b782a6f1d1ae6ff36ef2ea92b7a27f0f1
Pablo J <olbapjose@gmail.com>
2025-02-02 22:10:53 +0100

    Renombrado nombre en el setup.py para dejarlo con guiones medios

--- spark_flights_etl/setup.py
+++ spark_flights_etl/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="spark_flights_etl",
+    name="spark-flights-etl",
     version="0.1.0",
     author="Master Data Engineering UCM",
     author_email="alumno@ucm.es",
